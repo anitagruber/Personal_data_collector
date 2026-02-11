b36e316a337fce3bf1d0337f653f9994b59ff82e
ANI <revolutanita@gmail.com>
2026-02-11 15:11:31 +0100

    Refactor get_connection to use Streamlit secrets
    Updated get_connection function to use Streamlit secrets for database connection parameters.

--- database.py
+++ database.py
@@ -3,13 +3,23 @@ import streamlit as st
 
 def get_connection():
     connection = psycopg2.connect(
+        host=st.secrets["DB_HOST"],
+        database=st.secrets["DB_NAME"],
+        user=st.secrets["DB_USER"],
+        password=st.secrets["DB_PASSWORD"],
+        port=st.secrets["DB_PORT"]
+    )
+    return connection
+
+'''def get_connection():
+    connection = psycopg2.connect(
         host="localhost",
         database="postgres",
         user="postgres",
         password="postgres",                               #st.secrets["DB_PASSWORD"],
         port="5432"
     )
-    return connection
+    return connection'''
 
 def insert_personal(first_name,middle_name,last_name,date_of_birth,
                     gender,phone_number,email):
