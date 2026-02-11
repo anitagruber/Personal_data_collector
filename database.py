13c25571c61d14e1c97e4f0a20fc589b42713dda
ANI <revolutanita@gmail.com>
2026-02-11 15:52:40 +0100

    Refactor database connection with SSL options
    Refactored database connection to remove function and add SSL options.

--- database.py
+++ database.py
@@ -1,15 +1,16 @@
 import psycopg2
 import streamlit as st
 
-def get_connection():
-    connection = psycopg2.connect(
-        host=st.secrets["DB_HOST"],
-        database=st.secrets["DB_NAME"],
-        user=st.secrets["DB_USER"],
-        password=st.secrets["DB_PASSWORD"],
-        port=st.secrets["DB_PORT"]
-    )
-    return connection
+connection = psycopg2.connect(
+    host=st.secrets["DB_HOST"],
+    database=st.secrets["DB_NAME"],
+    user=st.secrets["DB_USER"],
+    password=st.secrets["DB_PASSWORD"],
+    port=st.secrets["DB_PORT"],
+    sslmode="require",
+    channel_binding="require"
+)
+
 
 '''def get_connection():
     connection = psycopg2.connect(
